9e9404cfc6ed67ae2000d5e1f2730d5a3f519396
Nubli Naim <nubli.naim@gmail.com>
2020-04-17 21:59:42 +0900

    chore: build into docs

---
+++ docs/precache-manifest.b03005c23c7af383e592f68e50661180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf0db247b6545fccc38da0d018ff841",
+    "url": "/flowerplanner/index.html"
+  },
+  {
+    "revision": "ec4654d5cd0e97f750a2",
+    "url": "/flowerplanner/static/css/main.babee896.chunk.css"
+  },
+  {
+    "revision": "1165afc10901617853db",
+    "url": "/flowerplanner/static/js/2.105ca39f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flowerplanner/static/js/2.105ca39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4654d5cd0e97f750a2",
+    "url": "/flowerplanner/static/js/main.a046cbb6.chunk.js"
+  },
+  {
+    "revision": "f2487393fbe564a2675d",
+    "url": "/flowerplanner/static/js/runtime-main.5915ce5e.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7045cf7e0cee05091dcc58cb8fda83e9.js"
+  "/flowerplanner/precache-manifest.b03005c23c7af383e592f68e50661180.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flowerplanner/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
