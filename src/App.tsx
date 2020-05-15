50c87573b631191f84cb300fe246219f7f7b18fa
Nubli <nubli.naim@gmail.com>
2020-05-15 19:10:13 +0900

    fix: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
-import FieldMaker from './FieldMaker';
-import IsabelleMaker from './IsabelleMaker/index';
-import FlowerExplainer from './FlowerExplainer';
-import FlowerCrosser from './FlowerCrosser';
 import FavChecker from './FavChecker';
 
 function App() {
-  const hash = window.location.hash;
   return <>
     <FavChecker/>
   </>;
